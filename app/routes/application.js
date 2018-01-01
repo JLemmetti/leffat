4ed6a0d63c1c214e782bf6591c67d228656b482a
Jurkka Lemmetti <jurkka.lemmetti@gmail.com>
2018-01-01 21:36:22 +0200

    Use spaces for indentation

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,10 +1,10 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-	beforeModel () {
-		return this.get('session').fetch().catch(function() {});
-	},
-	model () {
-		return this.store.findAll('movie');
-	}
+    beforeModel () {
+        return this.get('session').fetch().catch(function() {});
+    },
+    model () {
+        return this.store.findAll('movie');
+    }
 });
