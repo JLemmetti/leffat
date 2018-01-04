1ce61fe1560a6f13f826a2df3e01801beacf1fb1
Jurkka Lemmetti <jurkka.lemmetti@gmail.com>
2018-01-04 21:32:00 +0200

    Set current date as a default watch date for new movies

--- app/models/movie.js
+++ app/models/movie.js
@@ -4,6 +4,6 @@ export default DS.Model.extend({
   title: DS.attr('string'),
   link: DS.attr('string'),
   notes: DS.attr('string'),
-  watchDate: DS.attr('date'),
+  watchDate: DS.attr('date', {defaultValue: Date.now()}),
   editing: DS.attr('boolean', {defaultValue: false})
 });
