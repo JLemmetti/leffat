0b1ce2d640b2bcdf12cd836fd2f6bb8831483d2b
Jurkka Lemmetti <jurkka.lemmetti@gmail.com>
2018-01-08 19:04:03 +0200

    Don't send movie editing state to server

--- app/models/movie.js
+++ app/models/movie.js
@@ -4,6 +4,5 @@ export default DS.Model.extend({
   title: DS.attr('string'),
   link: DS.attr('string'),
   notes: DS.attr('string'),
-  watchDate: DS.attr('date', {defaultValue: Date.now()}),
-  editing: DS.attr('boolean', {defaultValue: false})
+  watchDate: DS.attr('date', {defaultValue: Date.now()})
 });
